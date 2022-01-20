5be20854cb97d4a762ea6aa26e1458b5c8659aab
jameel <jameelna978@gmail.com>
2022-01-20 18:56:25 +0200

    motio detect 0.2

--- motion_detect.m
+++ motion_detect.m
@@ -1,25 +1,17 @@
 close all;
-clear vars;
 clc;
-
-foregroundDetector = vision.ForegroundDetector('NumTrainingFrames', 10);
 
 cam = webcam(1);
 fig = figure;
 ax = subplot(2, 2, 1); 
-frame = rgb2gray(snapshot(cam)); 
+frame = snapshot(cam); 
 im = image(ax,zeros(size(frame),'uint8')); 
 axis(ax,'image');
 preview(cam,im)
 
-%get background
-for i = 1:10
-    frame = rgb2gray(snapshot(cam)); 
-    foreground = step(foregroundDetector, frame);
-end
-
-
-subplot(2, 2, 2);imshow(frame);
+base_frame = double(rgb2gray(snapshot(cam)))/255; 
+base_frame = imgaussfilt(base_frame,'FilterSize',21);
+subplot(2, 2, 2);imshow(base_frame);
 title('Current Foreground');
 
 blobAnalysis = vision.BlobAnalysis('AreaOutputPort', false, 'CentroidOutputPort', false, ...
@@ -30,35 +22,24 @@ frame_change = 0;
 
 while(1)
 pause(0.1);%get frame every 0.1 second
-frame = rgb2gray(snapshot(cam)); 
-foreground = foregroundDetector(frame);
-filteredForeground = medfilt2(foreground,[5 5]); 
-filteredForeground = medfilt2(filteredForeground,[5 5]); 
-filteredForeground = medfilt2(filteredForeground,[5 5]); 
+frame = double(rgb2gray(snapshot(cam)))/255; 
+frame = imgaussfilt(frame,'FilterSize',21);
 
-filted_mask = frame;
-filted_mask(~filteredForeground) = 0;
-subplot(2, 2, 3);imshow(filted_mask);title('Detected Movment');
+foreground = (abs(frame - base_frame)) > 0.1;
+filteredForeground = medfilt2(foreground,[5 5]); 
+filteredForeground = imfill(filteredForeground,'holes');
+
+
+subplot(2, 2, 3);imshow(filteredForeground);title('Detected Movment');
+
 bbox = step(blobAnalysis, filteredForeground);
 
-change = nnz(prev_filteredForeground)/nnz(filteredForeground + prev_filteredForeground);
-if(change > 0.9)
-    frame_change = frame_change + 1;
-else
-    frame_change = 0;
-end
-if(change > 0.9 && frame_change == 15)
-    foregroundDetector = vision.ForegroundDetector('NumTrainingFrames', 10);
-    for i = 1:10
-        frame = rgb2gray(snapshot(cam)); 
-        foreground = step(foregroundDetector, frame);
-    end
-    subplot(2, 2, 2);imshow(frame);
-    title('Current Foreground');
-end
-prev_filteredForeground = filteredForeground;
 
 
 result = insertShape(frame, 'Rectangle', bbox, 'Color', 'green');
 subplot(2, 2, 4); imshow(result); title('Detected Movment');
+
+base_frame = ((9.*base_frame)+frame)/10;
+subplot(2, 2, 2);imshow(base_frame);
+title('Current Foreground');
 end
