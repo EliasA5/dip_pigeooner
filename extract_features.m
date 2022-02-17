fcaf58bd644a9a0ef7527434e8fa12191fe498f9
Ghada <ghada.abuelzalaf@gmail.com>
2022-02-17 15:03:21 +0200

    added extract features and feature detect functions, saved the features data of the training inmages in extracted_features.mat file

---
+++ extract_features.m
@@ -0,0 +1,43 @@
+clear all
+clc
+
+load("bounding_boxes.mat");
+
+
+data_size = length(output.files);
+features_st = struct([]);
+
+for i = 1:data_size
+% for i = 1:10
+    file_name  = output.files(i);
+    img = imread(file_name);
+
+    img = rgb2gray(img);
+    bbox = output.bounding_boxes(i,:);
+    
+    points = detectSURFFeatures(img, "ROI", bbox, 'NumOctaves', 3,'NumScaleLevels', 3,'MetricThreshold',700);
+    [features,validPoints] = extractFeatures(img,points);
+    ex_features = struct('features', features, 'validPoints', validPoints); 
+    features_st = [features_st ,ex_features];
+
+    save('extracted_features.mat', 'features_st');
+    %load('extracted_features.mat', 'features_st')
+
+
+
+end
+
+
+
+% figure()
+% imshow(img);
+% hold on;
+% shape = insertShape(img,"Rectangle",bbox);
+% imshow(shape)
+
+
+% figure;
+% imshow(img);
+% hold on
+% plot(points)
+
