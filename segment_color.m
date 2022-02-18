1073650495316d7299d21734ff1608a7866ebb32
Elias Assaf <elyas51000@gmail.com>
2022-02-18 14:25:43 +0200

    faster version

--- segment_color.m
+++ segment_color.m
@@ -4,28 +4,23 @@ score = sum(mask, 'all') / numel(mask);
 end
 
 function [filter] = dip_find_pigeon(img)
-hsv = single(rgb2hsv(img));
+hsv = uint8(255*rgb2hsv(img));
+
 cap_h = hsv(:,:,1);
 cap_s = hsv(:,:,2);
 cap_v = hsv(:,:,3);
  
-cap_h_filt = zeros(size(cap_h), 'logical');
-cap_s_filt = zeros(size(cap_s), 'logical');
-cap_v_filt = zeros(size(cap_v), 'logical');
-cap_h_filt((cap_h >= 0.55) & (cap_h <= 0.62)) = 1;
-cap_s_filt((cap_s >= 0.1) & (cap_s <= 0.19)) = 1;
-cap_v_filt((cap_v >= 0.7) & (cap_v < 1)) = 1;
-
+cap_h_filt = (cap_h >= 140) & (cap_h <= 158); %140 158
+cap_s_filt = (cap_s >= 25) & (cap_s <= 49); %25 49
+cap_v_filt = (cap_v >= 179) & (cap_v <= 255); %179 255
 
 % figure;
 % subplot(1,3,1);imshow(cap_h_filt);title('h');
 % subplot(1,3,2);imshow(cap_s_filt);title('s');
 % subplot(1,3,3);imshow(cap_v_filt);title('v');
 
-
 filter = cap_h_filt & cap_s_filt & cap_v_filt;
 filter = imfill(filter, 4, 'holes');
 filter = medfilt2(filter,[5 5]);
-
 
 end
